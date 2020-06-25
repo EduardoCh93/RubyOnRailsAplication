d18aee21be7f2a3c3c4a549109ef67c0c21cdc60
EduardoCh93 <56979475+EduardoCh93@users.noreply.github.com>
2020-06-25 14:52:11 -0300

    Interfaz

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get home_path
+    assert_response :success
+    assert_select "title", "Home | Ruby on Rails Proyecto - Grupo 15"
+  end
+
+  test "should get help" do
+    get help_path
+    assert_response :success
+    assert_select "title", "Help | Ruby on Rails Proyecto - Grupo 15"
+  end
+
+  test "should get about" do
+    get about_path
+    assert_response :success
+    assert_select "title", "About | Ruby on Rails Proyecto - Grupo 15"
+  end
+
+end

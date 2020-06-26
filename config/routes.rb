897813f1be1bbc702abb1cabeddd5eb0a77287d1
Lucas Sotelo <lucasnsotelog@gmail.com>
2020-06-25 23:24:13 -0300

    Scaffolding de posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   #root'pages#home'
   get 'users/new'
   root 'static_pages#home'
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_24_200539) do
+ActiveRecord::Schema.define(version: 2020_06_26_005657) do
 
   create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false

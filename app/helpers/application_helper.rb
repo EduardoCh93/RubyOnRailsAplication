163bd1ad5a945de45ab5988d5ef148e77e6feff4
Lucas Sotelo <lucasnsotelog@gmail.com>
2020-06-25 23:24:49 -0300

    Fix cuando page_title era nil

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,10 @@
 module ApplicationHelper
-
   def full_title(page_title = '')
-    base_title = "Ruby on Rails Proyecto - Grupo 15"
-    if page_title.empty?
+    base_title = 'Ruby on Rails Proyecto - Grupo 15'
+    if page_title.blank?
       base_title
     else
-      page_title + " | " + base_title
+      "#{page_title} | #{base_title}"
     end
   end
-
 end

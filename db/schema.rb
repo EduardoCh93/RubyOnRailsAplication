70b6ef0797016cf299a653649e9ae3690c21ef3f
Lucas Sotelo <lucasnsotelog@gmail.com>
2020-07-06 20:41:55 -0300

    Modelo de post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_06_194758) do
+ActiveRecord::Schema.define(version: 2020_07_06_224307) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -22,11 +31,5 @@ ActiveRecord::Schema.define(version: 2020_07_06_194758) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
+  add_foreign_key "posts", "users"
 end

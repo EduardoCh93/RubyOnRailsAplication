174d012f0f14d41e865b3c203d1b9ace4303dda2
Lucas Sotelo <lucasnsotelog@gmail.com>
2020-07-06 23:27:52 -0300

    Mejores estilos para posts en perfil de usuario

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
 user = User.create!(name: 'lucas', email: 'lucas@example.com', password: '123456789', password_confirmation: '123456789')
+user.posts.create!(content: 'Erre con erre guitarra, erre con erre barril. Qué rápido ruedan las rápidas ruedas del rápido del ferrocarril.')
+user.posts.create!(content: "Un apostador de Río Negro ganó 43 millones de pesos en el Quini 6. El hombre se llevó el pozo que salió de la modalidad 'Segunda Vuelta'.")
 40.times { user.posts.create!(content: 'Hello world!') }
